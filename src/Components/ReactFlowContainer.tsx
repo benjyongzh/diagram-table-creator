8bff1691b375f32370e73582c90d64edf7014bf3
benjyongzh <benj_yongzh@hotmail.com>
2024-05-01 18:05:54 +0800

    Prep nodeTypes in reactflowcontainer.

--- src/Components/ReactFlowContainer.tsx
+++ src/Components/ReactFlowContainer.tsx
@@ -6,38 +6,47 @@ import {
   BackgroundVariant,
 } from "reactflow";
 import featureFlags from "Configs/featureFlags";
-import initialNodes from "Objects/initialNodes";
+import initialNodes, { initialNodeTypes } from "Objects/initialNodes";
 import initialEdges from "Objects/initialEdges";
 import { minimapStyle } from "Styles/minimap";
 import "reactflow/dist/style.css";
 
 import { useAppSelector, useAppDispatch } from "Hooks/reduxHooks";
-import { setNodes } from "Features/nodeSlice";
+import { setAllNodes, setAllNodeTypes } from "Features/nodeSlice";
+import { setAllEdges } from "Features/edgeSlice";
 
 const ReactFlowContainer = () => {
   const nodes = useAppSelector((state) => state.nodes.nodes);
+  const edges = useAppSelector((state) => state.edges.edges);
+  const nodeTypes = useAppSelector((state) => state.nodes.nodeTypes);
   const dispatch = useAppDispatch();
 
   const onInit = () => {
     console.log("hello! reactflow initialized");
     if (featureFlags.USE_INITIAL_OBJECTS) {
-      dispatch(setNodes(initialNodes));
+      dispatch(setAllNodes(initialNodes));
+      dispatch(setAllEdges(initialEdges));
+      dispatch(setAllNodeTypes(initialNodeTypes));
     }
   };
+
+  // const nodeTypes = useMemo(() => ({ textUpdater: TextUpdaterNode }), []);
 
   return (
     <ReactFlow
       nodes={nodes}
-      edges={featureFlags.USE_INITIAL_OBJECTS ? initialEdges : []}
+      edges={edges}
+      nodeTypes={nodeTypes}
       //   onNodesChange={onNodesChange}
       //   onEdgesChange={onEdgesChange}
       //   onConnect={onConnect}
       onInit={onInit}
+      // connectionLineComponent={}
       fitView
     >
       <MiniMap style={minimapStyle} zoomable pannable />
       <Controls />
-      <Background color="#ccc" variant={BackgroundVariant.Dots} gap={16} />
+      <Background color="#999" variant={BackgroundVariant.Dots} gap={16} />
     </ReactFlow>
   );
 };
