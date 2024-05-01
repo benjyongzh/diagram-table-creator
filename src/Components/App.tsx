0f190c7c1b8183dd090b9f7c56e6ff261c932c19
benjyongzh <benj_yongzh@hotmail.com>
2024-05-01 18:04:57 +0800

    Add sidebar.

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -2,12 +2,14 @@ import { ReactFlowProvider } from "reactflow";
 import ReactFlowContainer from "./ReactFlowContainer";
 import { store } from "Objects/store";
 import { Provider as ReduxProvider } from "react-redux";
+import { Sidebar } from "./Sidebar";
 
 const App = () => {
   return (
     <ReactFlowProvider>
       <ReduxProvider store={store}>
         <div className="main">
+          <Sidebar />
           <ReactFlowContainer />
         </div>
       </ReduxProvider>
