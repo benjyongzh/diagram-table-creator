72ceb0d46e26b75c1e26d8ee670dbd6e8aab339a
benjyongzh <benj_yongzh@hotmail.com>
2024-05-04 09:08:47 +0800

    use useMemo.

--- src/Components/CustomComponentNode.tsx
+++ src/Components/CustomComponentNode.tsx
@@ -2,9 +2,25 @@ import { memo } from "react";
 import { Handle } from "reactflow";
 import CustomNodeVariant from "Types/customNodeVariant";
 
-const CustomComponentNode = (data: CustomNodeVariant) => {
+// const CustomComponentNode = (data: CustomNodeVariant) => {
+//   return (
+//     <div>
+//       <div>CustomComponentNode {data.nodeName}</div>
+//       {data.handleTypes.map((handleType) =>
+//         Array.from({ length: handleType.quantity }).map((_item, index) => (
+//           <Handle
+//             id={`${data.nodeName} port ${index.toString()}`}
+//             type={handleType.handleType}
+//             position={handleType.position} //position should depend on value of handleCount
+//           />
+//         ))
+//       )}
+//     </div>
+//   );
+// };
+export default memo(({ data }: { data: CustomNodeVariant }) => {
   return (
-    <>
+    <div>
       <div>CustomComponentNode {data.nodeName}</div>
       {data.handleTypes.map((handleType) =>
         Array.from({ length: handleType.quantity }).map((_item, index) => (
@@ -15,8 +31,9 @@ const CustomComponentNode = (data: CustomNodeVariant) => {
           />
         ))
       )}
-    </>
+    </div>
   );
-};
+});
 
-export default memo(CustomComponentNode);
+// export default memo(CustomComponentNode);
+// export default CustomComponentNode;
