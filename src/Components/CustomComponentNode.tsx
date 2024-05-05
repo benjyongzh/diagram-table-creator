36994af0136971ab47b3a16c41b405d41305c4c7
benjyongzh <benj_yongzh@hotmail.com>
2024-05-05 17:36:22 +0800

    Create standard nodeStyling

--- src/Components/CustomComponentNode.tsx
+++ src/Components/CustomComponentNode.tsx
@@ -3,31 +3,35 @@ import { Handle, Node, NodeProps } from "reactflow";
 import CustomNodeVariant from "Types/customNodeVariant";
 
 export default memo(({ data }: { data: CustomNodeVariant }) => {
+  const onDeleteButtonClicked = () => {};
   return (
-    <div className="card">
-      <div className="card-body">
-        <h2 className="card-header">{data.nodeName}</h2>
-        <p className="text-content2">
-          {data.isHovered ? data.isHovered.toString() : "false"}
-        </p>
-        {/* <div className="card-footer">
-          <button className="btn btn-circle btn-error">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </button>
-        </div> */}
+    <div
+      className={`${
+        data.isHovered ? "bg-primary" : "bg-content2"
+      } rounded-md flex flex-col p-4 gap-3`}
+    >
+      <h2>{data.nodeName}</h2>
+      <p>{/*data.isHovered ? data.isHovered.toString() : "false"*/}</p>
+      <div className={`${data.isHovered ? "visible" : "invisible"}`}>
+        <button
+          className="btn btn-circle btn-error"
+          onClick={onDeleteButtonClicked}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
       </div>
       {data.handleTypes.map((handleType) =>
         Array.from({ length: handleType.quantity }).map((_item, index) => (
---
+++ src/Styles/node.ts
@@ -0,0 +1,3 @@
+export const nodeStyle = {
+  backgroundColor: "transparent",
+};
