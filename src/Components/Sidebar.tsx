349475fb71e124b6be40e551a505ad5a1f7b455e
benjyongzh <benj_yongzh@hotmail.com>
2024-05-12 22:36:31 +0800

    Implement better styling for buttons of component list items

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -14,7 +14,11 @@ export const Sidebar = () => {
   const [openedComponent, setOpenedComponent] =
     useState<CustomNodeVariant | null>(null);
 
-  const onComponentItemClick = (variant: CustomNodeVariant) => {
+  const onComponentItemClick = (variant: CustomNodeVariant | null) => {
+    if (variant === null) {
+      setOpenedComponent(null);
+      return;
+    }
     openedComponent === variant
       ? setOpenedComponent(null)
       : setOpenedComponent(variant);
--- src/Components/SidebarComponentListItem.tsx
+++ src/Components/SidebarComponentListItem.tsx
@@ -3,7 +3,8 @@ import { useAppDispatch } from "Hooks/reduxHooks";
 import { addNode } from "Features/reactFlowSlice";
 import { createNodeFromData } from "Utilities/reactFlowNodes";
 import { AccordionContent } from "./ui/accordion";
-import { ChevronDown } from "lucide-react";
+import { Plus, Pencil } from "lucide-react";
+import { Button } from "./ui/button";
 
 type sidebarComponentListItemProps = {
   variant: CustomNodeVariant;
@@ -17,8 +18,12 @@ export const SidebarComponentListItem = (
   const { variant, onComponentItemClick, isOpened } = props;
   const dispatch = useAppDispatch();
 
-  const onItemClick = () => {
-    onComponentItemClick(variant);
+  // const onItemClick = () => {
+  //   onComponentItemClick(variant);
+  // };
+
+  const isHovered = (bool: boolean) => {
+    onComponentItemClick(bool ? variant : null);
   };
 
   const onAdd = () => {
@@ -28,33 +33,43 @@ export const SidebarComponentListItem = (
   const onEdit = () => {};
 
   return (
-    <AccordionContent className="py-1 flex">
-      <button
-        className={`sidebar-list-menu-item hoverable-menu-item pl-8 flex transition-all ${
+    <AccordionContent
+      className="py-0 flex my-1"
+      onMouseEnter={() => isHovered(true)}
+      onMouseLeave={() => isHovered(false)}
+    >
+      <div
+        className={`cursor-default sidebar-list-menu-item hoverable-menu-item pl-8 flex transition-all ${
           isOpened ? "rounded-e-none" : ""
         }`}
-        onClick={onItemClick}
+        // onClick={onItemClick}
       >
-        <span className="menu-text text-base">{props.variant.nodeName}</span>
-        <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200 -rotate-90" />
-      </button>
+        <span className="menu-text text-base text-ellipsis">
+          {props.variant.nodeName}
+        </span>
+      </div>
       <div
-        className={`flex background-lower-contrast rounded-e-md overflow-hidden transition-all ease-in-out ${
-          isOpened ? "w-48" : "w-0"
+        className={`flex items-center justify-evenly background-lower-contrast rounded-e-md overflow-hidden transition-all ease-in-out ${
+          isOpened ? "w-36" : "w-0"
         }`}
       >
-        <button
-          className="flex items-center justify-center flex-1 menu-text bg-red-400"
+        <Button
           onClick={onAdd}
+          className="menu-text-low-contrast aspect-square rounded-full hover:bg-transparent dark:hover:bg-transparent hover:text-sky-600"
+          variant="ghost"
+          size="icon"
         >
-          Add
-        </button>
-        <button
-          className="flex items-center justify-center flex-1 menu-text background-lower-contrast"
+          <Plus className="h-6 w-6" />
+        </Button>
+
+        <Button
           onClick={onEdit}
+          className="menu-text-low-contrast aspect-square rounded-full hover:bg-transparent dark:hover:bg-transparent hover:text-sky-600"
+          variant="ghost"
+          size="icon"
         >
-          Edit
-        </button>
+          <Pencil className="h-5 w-5" />
+        </Button>
       </div>
     </AccordionContent>
   );
