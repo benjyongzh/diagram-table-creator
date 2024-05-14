a36ec6adc42753a73b14f5274a3c0fda477f9502
benjyongzh <benj_yongzh@hotmail.com>
2024-05-14 23:01:57 +0800

    Add content for settings modal. incomplete.

--- src/Components/ModalSettings.tsx
+++ src/Components/ModalSettings.tsx
@@ -1,9 +1,20 @@
 import { Modal } from "./Modal";
+import { Separator } from "./ui/separator";
 
 export const ModalSettings = () => {
   return (
     <Modal title="Settings">
-      <div>hello</div>
+      <Separator className="my-3" />
+      <div className="flex flex-col gap-5">
+        <section className="flex justify-between items-center w-full">
+          <header className="menu-text">Theme</header>
+          <span>dark mode?</span>
+        </section>
+        <section className="flex justify-between items-center w-full">
+          <header className="menu-text">Save</header>
+          <span>save/load</span>
+        </section>
+      </div>
     </Modal>
   );
 };
