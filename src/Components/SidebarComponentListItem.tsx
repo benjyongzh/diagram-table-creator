b5d2fa27ebad6c2bc51ca4916e884d41ab260d05
benjyongzh <benj_yongzh@hotmail.com>
2024-05-17 22:35:17 +0800

    Refactor for better encapsulation of react components

--- src/Components/SidebarComponentListItem.tsx
+++ src/Components/SidebarComponentListItem.tsx
@@ -2,10 +2,9 @@ import CustomNodeVariant from "Types/customNodeVariant";
 import { useAppDispatch } from "Hooks/reduxHooks";
 import { addNode } from "Features/reactFlowSlice";
 import { createNodeFromData } from "Utilities/reactFlowNodes";
-import { AccordionContent } from "./ui/accordion";
 import { Plus, Pencil } from "lucide-react";
-import { Button } from "./ui/button";
 import ButtonStyledIcon from "./ui/ButtonStyledIcon";
+import { SidebarListItem } from "./SidebarListItem";
 
 type sidebarComponentListItemProps = {
   variant: CustomNodeVariant;
@@ -19,10 +18,6 @@ export const SidebarComponentListItem = (
   const { variant, onHover, isFocused } = props;
   const dispatch = useAppDispatch();
 
-  // const onItemClick = () => {
-  //   onComponentItemClick(variant);
-  // };
-
   const isHovered = (bool: boolean) => {
     onHover(bool ? variant : null);
   };
@@ -34,37 +29,24 @@ export const SidebarComponentListItem = (
   const onEdit = () => {};
 
   return (
-    <AccordionContent
-      className="py-0 flex my-1"
-      onMouseEnter={() => isHovered(true)}
-      onMouseLeave={() => isHovered(false)}
-    >
-      <div
-        className={`cursor-default sidebar-list-menu-item hoverable-menu-item pl-8 flex transition-all ${
-          isFocused ? "rounded-e-none bg-slate-300 dark:bg-slate-700" : ""
-        }`}
-        // onClick={onItemClick}
-      >
-        <span
-          className={`menu-text text-base text-ellipsis ${
-            isFocused ? "font-medium" : ""
+    <SidebarListItem
+      text={props.variant.nodeName}
+      onHover={() => isHovered(true)}
+      isFocused={isFocused}
+      rightSideChildren={
+        <div
+          className={`flex items-center justify-evenly background-lower-contrast bg-slate-400 rounded-e-md overflow-hidden transition-all ease-in-out ${
+            isFocused ? "w-36" : "w-0"
           }`}
         >
-          {props.variant.nodeName}
-        </span>
-      </div>
-      <div
-        className={`flex items-center justify-evenly background-lower-contrast bg-slate-400 rounded-e-md overflow-hidden transition-all ease-in-out ${
-          isFocused ? "w-36" : "w-0"
-        }`}
-      >
-        <ButtonStyledIcon onButtonClick={onAdd}>
-          <Plus className="h-6 w-6" />
-        </ButtonStyledIcon>
-        <ButtonStyledIcon onButtonClick={onEdit}>
-          <Pencil className="h-5 w-5" />
-        </ButtonStyledIcon>
-      </div>
-    </AccordionContent>
+          <ButtonStyledIcon onButtonClick={onAdd}>
+            <Plus className="h-6 w-6" />
+          </ButtonStyledIcon>
+          <ButtonStyledIcon onButtonClick={onEdit}>
+            <Pencil className="h-5 w-5" />
+          </ButtonStyledIcon>
+        </div>
+      }
+    ></SidebarListItem>
   );
 };
--- src/Components/SidebarListItem.tsx
+++ src/Components/SidebarListItem.tsx
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { MouseEvent } from "react";
 import { AccordionContent } from "./ui/accordion";
 
 type sidebarComponentListItemProps = {
   text: string;
-  onHover: Function;
+  onHover?: Function;
+  onListItemClick?: Function;
   isFocused?: boolean;
-  children?: React.ReactNode;
+  leftSideChildren?: React.ReactNode;
+  rightSideChildren?: React.ReactNode;
 };
 
 export const SidebarListItem = (props: sidebarComponentListItemProps) => {
-  const { text, onHover, isFocused, children } = props;
+  const {
+    text,
+    onHover,
+    onListItemClick,
+    isFocused,
+    leftSideChildren,
+    rightSideChildren,
+  } = props;
 
-  const onItemClick = () => {};
-
-  const isHovered = (bool: boolean) => {};
+  const isHovered = (bool: boolean) => (onHover ? onHover(bool) : {});
+  const handleClick = (event: MouseEvent) =>
+    onListItemClick ? onListItemClick(event) : {};
 
   return (
     <AccordionContent
@@ -21,21 +30,22 @@ export const SidebarListItem = (props: sidebarComponentListItemProps) => {
       onMouseEnter={() => isHovered(true)}
       onMouseLeave={() => isHovered(false)}
     >
+      {leftSideChildren}
       <div
         className={`cursor-default sidebar-list-menu-item hoverable-menu-item pl-8 flex transition-all ${
           isFocused ? "rounded-e-none bg-slate-300 dark:bg-slate-700" : ""
         }`}
-        // onClick={onItemClick}
+        onClick={handleClick}
       >
         <span
           className={`menu-text text-base text-ellipsis ${
             isFocused ? "font-medium" : ""
           }`}
         >
-          {props.text}
+          {text}
         </span>
       </div>
-      {children}
+      {rightSideChildren}
     </AccordionContent>
   );
 };
