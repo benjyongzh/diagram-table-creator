3eccef7642fabf2856f4f58a044cbe58671fa593
benjyongzh <benj_yongzh@hotmail.com>
2024-05-18 21:54:14 +0800

    Extract out form fields

--- src/Components/ModalForm.tsx
+++ src/Components/ModalForm.tsx
@@ -10,27 +10,32 @@ import {
   FormItem,
   FormLabel,
 } from "./ui/form";
-// import { Switch } from "@/components/ui/switch";
 // import { toast } from "@/components/ui/use-toast";
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import formSchemaNewNode from "Objects/formSchemas/formSchemaNewNode";
+import React from "react";
+import { FormFieldsContainer } from "./FormFieldsContainer";
 
 type modalFormProps = {
   title: string;
+  schema: z.ZodObject<any>;
+  formFields: React.ReactNode;
+  onSubmit: Function;
 };
 
 export const ModalForm = (props: modalFormProps) => {
-  const form = useForm<z.infer<typeof formSchemaNewNode>>({
-    resolver: zodResolver(formSchemaNewNode),
+  const { title, schema, formFields, onSubmit } = props;
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
     defaultValues: {
       security_emails: true,
     },
   });
 
-  function onSubmit(data: z.infer<typeof formSchemaNewNode>) {
+  function onFormSubmit(data: z.infer<typeof schema>) {
+    onSubmit(data);
     // toast({
     //   title: "You submitted the following values:",
     //   description: (
@@ -41,62 +46,15 @@ export const ModalForm = (props: modalFormProps) => {
     // });
   }
   return (
-    <Modal title={props.title} width={640} isForm>
+    <Modal title={title} width={640} isForm>
       <Form {...form}>
         <form
-          onSubmit={form.handleSubmit(onSubmit)}
+          onSubmit={form.handleSubmit(onFormSubmit)}
           className="w-full space-y-6"
         >
           <div>
-            <h3 className="mb-4 text-lg font-medium">Email Notifications</h3>
-            <div className="space-y-4">
-              <FormField
-                control={form.control}
-                name="marketing_emails"
-                render={({ field }) => (
-                  <FormItem className="flex flex-row items-center justify-between rounded-lg border p-4">
-                    <div className="space-y-0.5">
-                      <FormLabel className="text-base">
-                        Marketing emails
-                      </FormLabel>
-                      <FormDescription>
-                        Receive emails about new products, features, and more.
-                      </FormDescription>
-                    </div>
-                    <FormControl>
-                      <Switch
-                        checked={field.value}
-                        onCheckedChange={field.onChange}
-                      />
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="security_emails"
-                render={({ field }) => (
-                  <FormItem className="flex flex-row items-center justify-between rounded-lg border p-4">
-                    <div className="space-y-0.5">
-                      <FormLabel className="text-base">
-                        Security emails
-                      </FormLabel>
-                      <FormDescription>
-                        Receive emails about your account security.
-                      </FormDescription>
-                    </div>
-                    <FormControl>
-                      <Switch
-                        checked={field.value}
-                        onCheckedChange={field.onChange}
-                        disabled
-                        aria-readonly
-                      />
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
-            </div>
+            <h3 className="mb-4 text-lg font-medium">{title}</h3>
+            <FormFieldsContainer form={form}>{formFields}</FormFieldsContainer>
           </div>
           <div className="w-full flex items-center justify-end gap-4">
             <Button type="submit" className="px-8">
