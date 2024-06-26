90f481bee7fbf901ff5d419ba3bb717a5699978b
benjyongzh <benj_yongzh@hotmail.com>
2024-06-26 22:40:26 +0800

    Fix: initial nodeVariants and handleVariants based on most updated schemas.

--- src/Data/Objects/initialNodes.ts
+++ src/Data/Objects/initialNodes.ts
@@ -4,6 +4,7 @@ import nodeConfigs from "@/Configs/nodeConfig";
 import { NodeVariant } from "Types/nodes/nodeVariant";
 import colors from "Types/colorString";
 import edgeConfig from "@/Configs/edgeConfig";
+import { HandleVariant } from "Types/handles/handleVariant";
 
 const initialNodes: Array<Node> = [
   {
@@ -21,52 +22,53 @@ export const initialNodeTypes: NodeTypes = {
   [nodeConfigs.INITIAL_CUSTOM_NODE_NAME]: CustomComponentNode,
 };
 
+export const initialHandleVariants: HandleVariant[] = [
+  {
+    id: "12345",
+    handleType: "source", //source | target
+    handleName: "bleh",
+    position: Position.Right,
+    quantity: 4,
+    edgeVariantId: edgeConfig.FREE_CONNECTION_TYPE_VARIANT_ID,
+  },
+  {
+    id: "67890",
+    handleType: "source", //source | target
+    handleName: "bla",
+    position: Position.Top,
+    quantity: 2,
+    edgeVariantId: "12345",
+  },
+
+  {
+    id: "abcde",
+    handleType: "source", //source | target
+    handleName: "blu",
+    position: Position.Bottom,
+    quantity: 8,
+    edgeVariantId: "67890",
+  },
+  {
+    id: "fghij",
+    handleType: "source", //source | target
+    handleName: "blihhh",
+    position: Position.Right,
+    quantity: 7,
+    edgeVariantId: "12345",
+  },
+];
+
 export const initialNodeVariants: Array<NodeVariant> = [
   {
     id: "12345",
     nodeName: "Monitoring Unit",
-    handleTypes: [
-      {
-        id: "12345",
-        handleType: "source", //source | target
-        handleName: "bleh",
-        position: Position.Right,
-        quantity: 4,
-        connectionType: edgeConfig.FREE_CONNECTION_TYPE_EDGE_IDENTIFIER,
-      },
-      {
-        id: "67890",
-        handleType: "source", //source | target
-        handleName: "bla",
-        position: Position.Top,
-        quantity: 2,
-        connectionType: "ET",
-      },
-
-      {
-        id: "abcde",
-        handleType: "source", //source | target
-        handleName: "blu",
-        position: Position.Bottom,
-        quantity: 8,
-        connectionType: "FO",
-      },
-    ],
+    handleTypes: ["12345", "67890", "abcde"],
     color: colors.orange,
   },
   {
     id: "67890",
     nodeName: "Extension Unit",
-    handleTypes: [
-      {
-        id: "fghij",
-        handleType: "source", //source | target
-        handleName: "blihhh",
-        position: Position.Right,
-        quantity: 7,
-        connectionType: "ET",
-      },
-    ],
+    handleTypes: ["fghij"],
     color: colors.blue,
   },
 ];
