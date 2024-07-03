b20a2c4ea03c1039a30661eed3978ac4138b5a7d
benjyongzh <benj_yongzh@hotmail.com>
2024-07-03 22:45:31 +0800

    Fix: edgeIdentifier definition includes free_connection_type

--- src/Data/Types/edges/edgeIdentifier.ts
+++ src/Data/Types/edges/edgeIdentifier.ts
@@ -4,11 +4,16 @@ import edgeConfig from "@/Configs/edgeConfig";
 const edgeIdentifierSchema = z
   .string()
   .regex(/^[a-zA-Z]*$/)
+  .min(
+    edgeConfig.EDGE_IDENTIFIER_MIN_LENGTH,
+    `Identifier must not be less than ${edgeConfig.EDGE_IDENTIFIER_MIN_LENGTH} characters long`
+  )
   .max(
     edgeConfig.EDGE_IDENTIFIER_MAX_LENGTH,
     `Identifier must not be more than ${edgeConfig.EDGE_IDENTIFIER_MAX_LENGTH} characters long`
   )
   .toUpperCase()
+  .or(z.literal(edgeConfig.FREE_CONNECTION_TYPE_EDGE_IDENTIFIER))
   .default(edgeConfig.FREE_CONNECTION_TYPE_EDGE_IDENTIFIER);
 
 export default edgeIdentifierSchema;
@@ -16,10 +21,11 @@ export default edgeIdentifierSchema;
 export type EdgeIdentifier = z.infer<typeof edgeIdentifierSchema>;
 
 export const isEdgeIdentifier = (arg: any): arg is EdgeIdentifier => {
-  return (
-    arg &&
-    arg.length() <= 2 &&
-    typeof arg === "string" &&
-    arg.match(/^[a-zA-Z]*$/)
-  );
+  if (
+    arg.length() <= edgeConfig.EDGE_IDENTIFIER_MIN_LENGTH ||
+    arg.length() >= edgeConfig.EDGE_IDENTIFIER_MAX_LENGTH
+  ) {
+    return arg === edgeConfig.FREE_CONNECTION_TYPE_EDGE_IDENTIFIER;
+  }
+  return arg && typeof arg === "string" && arg.match(/^[a-zA-Z]*$/);
 };
